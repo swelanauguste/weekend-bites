2b95d555f77038718db9fd02f8a44cdb22943556
Swelan Auguste <swelanauguste@gmail.com>
2023-07-12 09:29:05 -0400

    is_available

--- app/core/views.py
+++ app/core/views.py
@@ -34,7 +34,7 @@ class ProductListView(ListView):
                 is_available=True,
             ).distinct()
         else:
-            return Product.objects.all()
+            return Product.objects.all(is_available=True)
 
 
 class ProductDetailView(DetailView):
